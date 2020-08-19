067fe0be491223d45b56eca0b443a38e79e1bd09
Geovane Feitosa Cavalcante <geovanefeitosacavalcante@gmail.com>
2020-08-19 20:53:15 -0300

    inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { EducationComponent } from './education/education.component';
+import { SkillsComponent } from './skills/skills.component';
+import { InterestsComponent } from './interests/interests.component';
+import { AwardsComponent } from './awards/awards.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ExperienceComponent,
+    EducationComponent,
+    SkillsComponent,
+    InterestsComponent,
+    AwardsComponent
   ],
   imports: [
     BrowserModule,
